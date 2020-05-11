f78ea03eede8c315061f0925592462ab156aa305
mylenavanessa <mylenavanessa@hotmail.com>
2020-05-11 03:57:34 -0300

    first test picker

--- src/screen/SingUp/index.js
+++ src/screen/SingUp/index.js
@@ -1,5 +1,7 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import { Picker } from 'react-native'
 import api from '../../services/api'
+import location from '../../services/location'
 
 import { 
           Title,
@@ -25,6 +27,19 @@ export default function SingUp({ navigation }) {
   const [password, setPassword] = useState('')
   const [phone, setPhone] = useState('')
   const [username, setUsername] = useState('')
+  const [state, setState] = useState(null)
+  const [city, setCity] = useState(null)
+  const [stateList, setStateList] = useState([])
+
+  useEffect(() => {
+    async function fetchStates(){ 
+      console.log('entrou')
+      const {data} = await location.get('/estados')
+      console.log(data)
+      setStateList(data)
+    }
+    fetchStates();
+  }, [])
 
   async function handleSingUp() {
     setLoading(true);
@@ -88,6 +103,20 @@ export default function SingUp({ navigation }) {
                   value={phone}
                   onChangeText={setPhone}
                 />
+                <Picker
+                  placeholder='Escolha um estado'
+                  selectedValue={state}
+                  style={{height: 50, width: 100}}
+                  onValueChange={(itemValue, itemIndex) =>
+                    setState(itemValue)
+                  }>
+                    {
+                      stateList.length > 0 &&
+                      stateList.map(item => 
+                        <Picker.Item key={item.id} label={item.nome} value={item.nome} />
+                      )
+                    }
+                </Picker>
               </ContainerSigUp>
               <ButtomTouchableOpacity onPress={handleSingUp}>
                 <Title_text>Criar Conta</Title_text>
@@ -95,8 +124,9 @@ export default function SingUp({ navigation }) {
 
               <ContainerLogin>
                 <Title_text_login>Já possui conta?</Title_text_login>
-                <Text_login onPress={() => navigation.navigate('Login')}
-                >Entre agora</Text_login>
+                <Text_login onPress={() => navigation.navigate('Login')}>
+                  Entre agora
+                </Text_login>
               </ContainerLogin>
             </ContainerInsideKeyboard>
           </ContainerKeyboard>
